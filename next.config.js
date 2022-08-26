c3f7c71db0f3cf28623f393c867d49334a510d5f
Seokyun Ha <bluehornblues@gmail.com>
2022-08-26 17:28:34 +0900

    setup sass

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')], // 2. sassOptions 옵션 추가
+  },
 }
 
 module.exports = nextConfig

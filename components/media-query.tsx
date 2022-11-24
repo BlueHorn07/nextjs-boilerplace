dc5b0956adfeefa4f8ab44c69fd0bb6de51ec39d
bluehorn07 <bluehornblues@gmail.com>
2022-11-24 23:30:18 +0900

    add media-query wrapper component

---
+++ components/media-query.tsx
@@ -0,0 +1,36 @@
+import React, {ReactNode} from 'react'
+import styled from "styled-components";
+
+export const Mobile  = ({ children, criteria }: {
+  children: ReactNode,
+  criteria: string
+}) => {
+  return (
+    <MobileWrapper criteria={criteria}>
+      {children}
+    </MobileWrapper>
+  )
+}
+
+const MobileWrapper = styled.div<{criteria: string}>`
+  @media only screen and (min-width: ${props => props.criteria}) {
+    display: none;
+  }
+`
+
+export const Desktop  = ({ children, criteria }: {
+  children: ReactNode
+  criteria: string
+}) => {
+  return (
+    <DesktopWrapper criteria={criteria}>
+      {children}
+    </DesktopWrapper>
+  )
+}
+
+const DesktopWrapper = styled.div<{criteria: string}>`
+  @media only screen and (max-width: ${props => props.criteria}) {
+    display: none;
+  }
+`
